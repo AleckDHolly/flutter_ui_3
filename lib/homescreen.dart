075376cb47e39be13d7daecabf6112de8bc67589
Aleck David Holly <aleckdavidholly@gmail.com>
2023-11-04 22:21:53 -0400

    top part does not move

--- lib/homescreen.dart
+++ lib/homescreen.dart
@@ -12,65 +12,56 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        bottom: false,
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const Padding(
-                padding: EdgeInsets.all(8.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        // bottom: false,
+        child: Column(
+          children: [
+            const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: TopPart(),
+            ),
+            Expanded(
+              child: SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    TopPart(),
-                    CircleAvatar(
-                      backgroundColor: Color.fromARGB(62, 158, 158, 158),
-                      child: Padding(
-                        padding: EdgeInsets.all(8.0),
-                        child: Icon(
-                          Icons.notifications_outlined,
-                          color: Colors.black,
+                    const Padding(
+                      padding: EdgeInsets.all(10),
+                      child: Text(
+                        "Subscriptions",
+                        style: TextStyle(
+                          fontSize: 30,
+                          letterSpacing: -2,
+                          fontWeight: FontWeight.w500,
                         ),
                       ),
-                    )
+                    ),
+                    const Subs(),
+                    const Categories(),
+                    MainArea(
+                      image: "images/1.jpg",
+                      text: "Video 1",
+                    ),
+                    MainArea(
+                      image: "images/2.jpg",
+                      text: "Video 1",
+                    ),
+                    MainArea(
+                      image: "images/3.jpg",
+                      text: "Video 1",
+                    ),
+                    MainArea(
+                      image: "images/4.jpg",
+                      text: "Video 1",
+                    ),
+                    MainArea(
+                      image: "images/5.jpg",
+                      text: "Video 1",
+                    ),
                   ],
                 ),
               ),
-              const Padding(
-                padding: EdgeInsets.all(10),
-                child: Text(
-                  "Subscriptions",
-                  style: TextStyle(
-                    fontSize: 30,
-                    letterSpacing: -2,
-                    fontWeight: FontWeight.w500,
-                  ),
-                ),
-              ),
-              const Subs(),
-              const Categories(),
-              MainArea(
-                image: "images/1.jpg",
-                text: "Video 1",
-              ),
-              MainArea(
-                image: "images/2.jpg",
-                text: "Video 1",
-              ),
-              MainArea(
-                image: "images/3.jpg",
-                text: "Video 1",
-              ),
-              MainArea(
-                image: "images/4.jpg",
-                text: "Video 1",
-              ),
-              MainArea(
-                image: "images/5.jpg",
-                text: "Video 1",
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
--- lib/widgets/top_part.dart
+++ lib/widgets/top_part.dart
@@ -6,26 +6,41 @@ class TopPart extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
+        Row(
+          children: [
+            CircleAvatar(
+              backgroundImage: AssetImage("images/avatar.jpg"),
+            ),
+            Padding(
+              padding: EdgeInsets.only(left: 8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Welcome Back 👋",
+                    style: TextStyle(color: Colors.grey),
+                  ),
+                  Text(
+                    "Aleck D.",
+                    style: TextStyle(fontWeight: FontWeight.w700),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
         CircleAvatar(
-          backgroundImage: AssetImage("images/avatar.jpg"),
-        ),
-        Padding(
-          padding: EdgeInsets.only(left: 8.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                "Welcome Back 👋",
-                style: TextStyle(color: Colors.grey),
-              ),
-              Text(
-                "Aleck D.",
-                style: TextStyle(fontWeight: FontWeight.w700),
-              ),
-            ],
+          backgroundColor: Color.fromARGB(62, 158, 158, 158),
+          child: Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Icon(
+              Icons.notifications_outlined,
+              color: Colors.black,
+            ),
           ),
-        ),
+        )
       ],
     );
   }
